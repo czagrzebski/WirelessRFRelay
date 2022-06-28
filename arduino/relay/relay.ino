392670cb26536974b9f207e61347278f7e0554c5
czagrzebski <48450104+czagrzebski@users.noreply.github.com>
2022-06-27 22:40:08 -0500

    Added EEPROM Recovery

--- arduino/relay/relay.ino
+++ arduino/relay/relay.ino
@@ -1,13 +1,8 @@
-#include <nRF24L01.h>
-#include <printf.h>
-#include <RF24_config.h>
-#include <RF24.h>
-
 /*
-  * Wireless nRF24L01 Relay Receiver
-  * 
-  * Author: Creed Zagrzebski (czagrzebski)
-  */
+* Wireless nRF24L01 Relay Receiver
+* 
+* Author: Creed Zagrzebski (czagrzebski)
+*/
 
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -15,13 +10,11 @@
 #include <EEPROM.h>
 
 // Map Arduino Pins to Relays
-int relay_pins[4] = {2, 3, 4, 5}; 
+int relay_pins[4] = {5, 4, 3, 2}; 
 
 RF24 radio(7, 8); //(CE - Pin 7, CSN - Pin 8)
 
 uint8_t address[][6] = {"1Node"};
-
-bool radioNumber = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
 
 struct data
 {
@@ -36,11 +29,11 @@ Data payload;
 void setup()
 { 
     //For serial debugging
-    Serial.begin(11520);
+    Serial.begin(115200);
 
     radio.begin();
 
-    Serial.println("Wirelss Relay Receiver");
+    Serial.println("Wireless Relay Receiver v1.0.0");
 
     //Prevents Power Supply Related Issue (and isn't necessary for testing when they are close together)
     radio.setPALevel(RF24_PA_LOW);
@@ -49,22 +42,28 @@ void setup()
     radio.setPayloadSize(8); // Data from RPi is 8 bytes ( 4-byte ints)
 
     // set the RX address of the TX node into a RX pipe
-    radio.openReadingPipe(1, address[0]); // using pipe 1
+    radio.openReadingPipe(0, address[0]); // using pipe 1
 
-    radio.startListening(); // put radio in RX mode
+    // put radio in RX mode
+    radio.startListening(); 
 
-    //Setup All Defined Relay Pins
+    //Setup All Defined Relay Pins and restore last state from EEPROM
     for (int i = 0; i < sizeof(relay_pins); i++)
     {
         pinMode(relay_pins[i], OUTPUT);
-        digitalWrite(relay_pins[i], HIGH); //start off
+        if(EEPROM.read(i) == 1){
+            digitalWrite(relay_pins[i], HIGH);
+        } else {
+            digitalWrite(relay_pins[i], LOW);
+        }
+      
     }
 }
 
 void loop()
 {
     uint8_t pipe;
-
+  
     if (radio.available(&pipe))
     {
         uint8_t bytes = radio.getPayloadSize(); // get the size of the payload
@@ -74,8 +73,7 @@ void loop()
         Serial.print(bytes); // size of payload
         Serial.print(F(" bytes on pipe "));
         Serial.print(pipe); // print the pipe number
-        Serial.println(payload.state);
-
+     
         //Relay_ID of -1 represents all relays (Allows the user to Turn on/off all relays at once)
         if (payload.relay_id == -1)
         {
@@ -99,43 +97,51 @@ void loop()
         }
         else
         {
-            //Translate Relay Number to Relay PIN on Arduino
-            int RELAY_PIN = relay_pins[payload.relay_id - 1];
-
             if (payload.state == 1)
             {
-                digitalWrite(RELAY_PIN, HIGH);
+                toggleOn(payload.relay_id);
             }
-            else if (payload.state == 0)
+            else 
             {
-                digitalWrite(RELAY_PIN, LOW);
+                toggleOff(payload.relay_id);
             }
         }
     }
-
-    delay(100);
 }
 
+/**
+ * Changes relay state to LOW
+ * 
+ * @param relay_id Relay ID
+ */
 void toggleOff(int relay_id){
     //Translate Relay Number to Relay PIN on Arduino
-    int RELAY_PIN = relay_pins[payload.relay_id - 1];
+    int RELAY_PIN = relay_pins[relay_id - 1];
     digitalWrite(RELAY_PIN, LOW);
     saveRelayState(relay_id, 0);
 }
   
+/**
+ * Changes relay state to HIGH
+ * 
+ * @param relay_id Relay ID 
+ */
 void toggleOn(int relay_id){
     //Translate Relay Number to Relay PIN on Arduino
-    int RELAY_PIN = relay_pins[payload.relay_id - 1];
+    int RELAY_PIN = relay_pins[relay_id - 1];
     digitalWrite(RELAY_PIN, HIGH);
     saveRelayState(relay_id, 1);
 }
 
 /**
- * Saves the relay state to EEPROM
+ * Saves the relay state to EEPROM. Safety feature for
+ * unexpected power outages. 
  * 
  * @param relay_id The ID of the relay
  * @param state  The state of the relay
  */
 void saveRelayState(int relay_id, int state){
+    Serial.println(relay_id);
+    Serial.println(state);
     EEPROM.update(relay_id - 1, state);
 }
